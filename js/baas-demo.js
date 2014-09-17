dd009cec30ae81e5bda2348c2c3ad56c91786932
Chris Latimer <clatimer@apigee.com>
2014-09-16 23:16:58 -0400

    added demo app files

---
+++ js/baas-demo.js
@@ -0,0 +1,19 @@
+<!-- STEP 2: Add API BaaS Connection -->
+  <!-- Add step 2 code here -->
+<!-- End STEP 2 -->
+
+<!-- STEP 4: Add loadUsers() function -->
+  <!-- Add loadUsers code here -->
+<!-- End STEP 4 -->
+
+<!-- STEP 6: Add EventListeners -->
+  <!-- Add event listener code here -->
+<!-- End STEP 6 -->
+
+<!-- STEP 7: Add createRelationship() function -->
+  <!-- Add createRelationship code here -->
+<!-- End STEP 7 -->
+
+<!-- STEP 8: Add createActivitySteam() function -->
+  <!-- Add createActivitySteam code here -->
+<!-- End STEP 8 -->
